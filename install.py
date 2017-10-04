e3e375b0b39ca139975ef7bf3763d90d6c686b51
Joshua Moore <joshuamoore235@gmail.com>
2017-10-04 14:00:00 -0600

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,6 @@
 #Python
 import os
 
-print("Running installer for Google Earth Pro...")
-os.system("GoogleEarthProSetup.exe")
 print("Running installer for SimpleKML...")
 os.system("pip install simplekml")
 print("Completed installation.")
